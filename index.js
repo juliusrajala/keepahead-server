131a001484d0324952aaaac8f878e6ebdf97ea94
Julius Rajala <juliusrajala@gmail.com>
2015-12-04 21:54:40 +0200

    Made server actually do something.

--- index.js
+++ index.js
@@ -5,14 +5,19 @@ var path = require('path');
 var app = express();
 
 var JSON_FILE_PATH = "Something";
+var PORT = 3000;
 
 //HTTP POST callback for receiving data
 app.post("/hot4guysapi/post", function(req, res){
 	console.log(req.body);
-	console.log("End");
+	console.log("HTTP POST done.");
 });
 
 //HTTP GET callback for delivering data
 app.get("/getlocation", function(req, res){
-
+	console.log("Someone tried to get stuff.");
 });
+
+console.log("Listening to port "+ PORT);
+
+app.listen(3000);
