72b8d78a95817a17b343c993ff5560cc05798b22
Julius Rajala <juliusrajala@gmail.com>
2015-12-04 19:22:27 +0200

    Built initial index.js and stuff

--- index.js
+++ index.js
@@ -4,3 +4,13 @@ var path = require('path');
 // var PythonShell = require('python-shell');
 var app = express();
 
+var JSON_FILE_PATH = "Something";
+
+//Post callback for receiving data
+app.post("/postlocation", function(){
+
+});
+
+app.get("/getlocation/", function(){
+
+});
