d7af35750ccb23db9d66f3a83592831f2b1b5346
Julius Rajala <juliusrajala@gmail.com>
2015-12-05 11:13:47 +0200

    I am not writing python here, but I am an idiot.

--- index.js
+++ index.js
@@ -32,10 +32,10 @@ function startAPI(settings) {
       console.log(dData);
       console.log(sensData);
       console.log("----------------Clearing data---------------");
-      for(var sensor in sensData){
-        console.log("sensor value: " + sensor);
-        if(value.sId == "0x00060100"){
-          temperature = value.val;
+      for(var key in sensData){
+        console.log("sensor value: " + sensData[key]);
+        if(sensData[key].sId == "0x00060100"){
+          temperature = sensData[key].val;
           console.log("Temperature set to: " + temperature);
         }
       }
